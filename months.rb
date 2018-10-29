96946a702a3c16304f6320f25eee81ad46455453
Tvisterman <44491682+Tvisterman@users.noreply.github.com>
2018-10-29 12:26:52 +0600

    Delete months.rb

--- months.rb
+++
@@ -1,9 +0,0 @@
-months = {:jan => "January 31", :feb => "February 28", :mar => "March 31", :apr => "April 30", :may => "May 31", :jun => "June 30", :jul => "July 31", :aug => "August 31", :sep => "September 30", :oct => "October 31", :nov => "November 30", :dec => "December 31"}
-
-puts months[:apr]
-
-puts months[:jun]
-
-puts months[:sep]
-
-puts months[:nov]
