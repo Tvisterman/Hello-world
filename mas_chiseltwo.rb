383f0ce1737ec9eec3e90b8c9dd1066e7354bcd5
Tvisterman <44491682+Tvisterman@users.noreply.github.com>
2018-11-04 15:24:25 +0600

    Add files via upload

---
+++ mas_chiseltwo.rb
@@ -0,0 +1,10 @@
+def tvist(dar)
+  dar.min.length
+  dur = dar.min.length
+  dar.max.length
+  der = dar.max.length
+  dur + der
+  dir = dur + der
+  puts dir
+end
+tvist ['vita', 'vit', 'vi', 'vitalenok']
---
+++ max_chislo.rb
@@ -0,0 +1,6 @@
+def minmax(str)
+  numbers = str.split(' ')
+  puts numbers.min
+  puts numbers.max
+end
+minmax ('1 2 3 4')
