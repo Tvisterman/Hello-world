e78fdbae3122ea89b131cbfb17b1a245137e1149
Tvisterman <44491682+Tvisterman@users.noreply.github.com>
2018-11-14 12:30:44 +0600

    Add files via upload

--- radius_darts.rb
+++ radius_darts.rb
@@ -1,14 +1,23 @@
 def score_throws(radius = [])
-    score = 0
-    radius.each do |r|
-    if r < 5
-    score = score + 10
-    elsif r >= 5 && r <= 10
-    score = score + 5
-    return "0" if radius == empty
-     end
-  end
-  p score
-end
+  return "0" if radius.empty?
+  score = 0
 
-score_throws []
+  radius.each do |r|
+    if r < 5
+      score = score + 10
+    elsif r >= 5 && r <= 10
+      score = score + 5
+    end
+  end
+  r2 = []
+  radius.each do |i|
+    if i < 5
+       r2 << 1
+    else
+       r2 << 0
+      end
+    end
+    score += 100 unless r2.include? (0)
+    p score
+  end
+score_throws [4,4,4]
